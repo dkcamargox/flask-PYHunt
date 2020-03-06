84295078346b3f6ba011f085d113677823b4d25c
Douglas Camargo <camargo.douglas@icloud.com>
2020-03-06 04:28:17 -0300

    add the button methods

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,26 +1,49 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 import json
 import requests
 TEMPLATE_DIR='./templates'
-
 STATIC_DIR='./static'
 app = Flask(__name__,template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
-URL = 'http://localhost:3001/api/products'
-request = requests.get(url=URL)
-data = request.json()['docs']
-print(data)
 
+
+def requisistar( page ):
+    response = []
+    __URL = 'http://localhost:3001/api/products?page='+ str(page)
+    request = requests.get(url=__URL)
+    pages = request.json()['pages']
+    data = request.json()['docs']
+    response.append(pages)
+    response.append(data)
+    return response
+
+def enabled_disabled(truFals):
+    if(truFals):
+        return("disabled")
+    else:
+        return("enabled")
+
+
+#routes 
+funcs = {
+    "en_dis": enabled_disabled,
+    "rd": redirect
+}
 
 @app.route("/")
 @app.route("/home")
 def home_page():
-    return render_template('home.html', data=data)
+    page = int(request.args.get('page', '1'))
+    print(funcs['en_dis'])
+    # api retorna já paginado, a variavel page define a pagina
+    return render_template('home.html', data=requisistar(page)[1], 
+    page=page, pages=requisistar(page)[0], functions=funcs)
 
 
 @app.route("/about")
 def about_page():
     return render_template('about.html')
 
+# run 
 
 if __name__ == '__main__':
     app.run(debug=1)
